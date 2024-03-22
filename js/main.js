f36d250282d76864f240eec1ec24f7cf378b9670
lufucx <luizsilva.miui@gmail.com>
2024-03-22 12:52:51 -0300

    função de adicionar funcionando

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function adicionarTituloTarefa() {
+    var tituloTarefa = document.getElementById('titulo').value.trim();
+    if (tituloTarefa === '') {
+        alert("Por favor, insira o título da tarefa!");
+        return;
+    }
+    
+    var novoTituloTarefa = document.createElement('li');
+    novoTituloTarefa.textContent = tituloTarefa;
+
+    document.getElementById('lista').appendChild(novoTituloTarefa);
+
+    document.getElementById('titulo').value = '';
+  }
+
+  function adicionarDescricaoTarefa() {
+    var descricaoTarefa = document.getElementById('descricao').value.trim();
+    if (descricaoTarefa === '') {
+        alert("Por favor, insira a descrição da tarefa!");
+        return;
+    }
+    
+    var novaDescricaoTarefa = document.createElement('li');
+    novaDescricaoTarefa.textContent = descricaoTarefa;
+
+    document.getElementById('lista').appendChild(novaDescricaoTarefa);
+
+    document.getElementById('descricao').value = '';
+  }
+
+  document.getElementById('adicionarBtn').addEventListener('click', function() {
+    adicionarTituloTarefa();
+    adicionarDescricaoTarefa();
+  });
